4e2a104115594c55584806b5099fc20a6b8faa0d
ferga <fergalmiller@hotmail.co.uk>
2024-05-02 18:16:08 +0100

    Expense now allows an optional dated field

--- src/main/kotlin/Expense.kt
+++ src/main/kotlin/Expense.kt
@@ -1,1 +1,32 @@
-data class Expense(val value: Int, private val description: String)
+import java.time.LocalDate
+
+/**
+ * Represents an associated release expense, e.g. the cost of mastering a track.
+ * This can be used to track label expenditure on a release and be included in payout calculations.
+ */
+data class Expense(
+    /**
+     * The value in minor amount - e.g. £1 = `100`
+     */
+    val value: Int,
+    /**
+     * An optional description of the expense - e.g. `"Track Mastering"`
+     */
+    private val description: String,
+    /**
+     * An optional date associated with the expense.
+     * Used in payout calculation to ensure that expense values are applied at the correct point in time.
+     * Null value implies this is a general expense, so should be applied to calculations from the very beginning of sales.
+     */
+    val date: LocalDate? = null
+)
+
+fun List<Expense>.getExpenseValueUpTo(until: LocalDate): Int {
+
+    return this.filter { it.date?.isBefore(until) ?: true }.sumOf { it.value }
+}
+
+fun List<Expense>.getTotalExpenseValueBetweenDates(from: LocalDate, untilInclusive: LocalDate): Int {
+
+    return this.filter { it.date != null  && it.date.isAfter(from) && (it.date.isEqual(untilInclusive) || it.date.isBefore(untilInclusive)) }.sumOf { it.value }
+}
